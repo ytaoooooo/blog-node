ee9db7b1265372674b0cc97c8102ed2762668385
杨涛 <1347781778@qq.com>
2020-02-21 17:20:53 +0800

    this is a test

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const app = new Koa()
 const router = new Router()
 
 
-router.get('/api/test',(ctx,next)=>{
+router.get('/node/api/test',(ctx,next)=>{
     ctx.body = {
-        key: 'test'
+        "code": "200",
+        "msg" : "success",
+        "data": {
+            "articleList": [
+                {
+                    "title":"关于网站，关于博客",
+                    "img": "https://yangicheng.cn/static/image/blog-summary/javascript.png",
+                    "summary": ["心路历程","闲谈"]
+                },
+                {
+                    "title":"浅谈https",
+                    "img": "https://yangicheng.cn/static/image/blog-summary/javascript.png",
+                    "summary": ["理论","面试"]
+                }
+            ]
+        }
     }
 })
 
