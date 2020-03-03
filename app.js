a8ab0e9727b0540952d2bc263e69a160373afd5a
杨涛 <1347781778@qq.com>
2020-03-03 09:49:17 +0800

    tewst

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const Koa = require('koa')
 
 const app = new Koa()
 const {InitManager} = require('./core/init')
+const parser = require('koa-bodyparser')
 
 
 
-
-
+app.use(parser())
 InitManager.initCore(app)
 
 
--- app/api/v1/login.js
+++ app/api/v1/login.js
@@ -2,13 +2,21 @@ const Router = require('koa-router')
 const router = new Router()
 
 router.post('/node/api/v1/login', (ctx, next) => {
+    const account = 'CIM'
+    const password = '123456'
     const data = ctx.request.body
+    let msg = 'success'
+    if(data.account !== account){
+        msg = '账号不存在'
+    }
+    if(data.account === account && data.password != password){
+        msg = '密码不正确'
+    }
+    
     ctx.body = {
         "code": "200",
-        "msg": "success",
-        "data": {
-            data
-        }
+        "msg": msg,
+        "data": {}
     }
 })
 
