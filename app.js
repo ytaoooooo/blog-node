8096a645a853f83722a6fbbb5e868ccf2ea459b4
杨涛 <1347781778@qq.com>
2020-03-03 11:08:52 +0800

    test

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const Koa = require('koa')
 var cors = require('koa2-cors');
 const app = new Koa()
 const {InitManager} = require('./core/init')
-const parser = require('koa-bodyparser')
 
 
-app.use(cors());
+
 app.use(parser())
 InitManager.initCore(app)
 
